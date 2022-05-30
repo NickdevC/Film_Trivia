3bb9d0a65da4142d77702bbed6521ced61420ee7
NickdevC <nick_coles@msn.com>
2022-05-30 15:57:25 +0000

    Display question box when 'start' is clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,22 @@
 
-const startButton = document.getElementById('begin-btn')
-const rulesContainerElement = document.getElementById('rules-box')
 
-startButton.addEventListener('click', startGame)
+
+const beginButton = document.getElementById('begin-btn')
+const rulesContainerElement = document.getElementById('rules-box')
+const startButton = document.getElementById('start-btn')
+const questionContainerElement = document.getElementById('question-box')
+
+beginButton.addEventListener('click', startGame)
+startButton.addEventListener('click', firstQuestion)
 
 function startGame() {
-    console.log('started');
-    startButton.classList.add('hide');
+    beginButton.classList.add('hide');
     rulesContainerElement.classList.remove('hide');
+}
+
+function firstQuestion() {
+    rulesContainerElement.classList.add('hide');
+    questionContainerElement.classList.remove('hide');
 }
 
 function setNextQuestion() {
