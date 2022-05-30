afe91d4198f66a8367b689b2f740ac4b47266cfc
NickdevC <nick_coles@msn.com>
2022-05-30 19:06:16 +0000

    Add initial questions and answers to array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,22 @@
-
-
+const quizQuestions = [
+    {
+      question: "In which film did Ellen Ripley first appear?",
+      answers: ["Casino Royale", "Alien", "Titanic", "Forest Gump"],
+      correct: "Alien"
+    },
+  
+    {
+      question: 'Finish the famous movie line: "We\'re going to need. bigger ______"',
+      answers: ["car", "loan", "hammer", "boat"],
+      correct: "boat"
+    },
+    
+    {
+      question: "What is the name of the skyscraper in Die Hard?",
+      answers: ["Noki Plaza", "Niagra Plaza", "Nakatomi Plaza", "Nakamati Plaza"],
+      correct: "Nakatomi Plaza"
+    },
+]
 
 const beginButton = document.getElementById('begin-btn')
 const rulesContainerElement = document.getElementById('rules-box')
@@ -17,6 +34,7 @@ function startGame() {
 function firstQuestion() {
     rulesContainerElement.classList.add('hide');
     questionContainerElement.classList.remove('hide');
+    setNextQuestion();
 }
 
 function setNextQuestion() {
